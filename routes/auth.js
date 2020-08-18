f195559915e8043b353cf76ccd213ade59b45f6a
Vertipae <33002993+Vertipae@users.noreply.github.com>
2020-08-18 19:08:23 +0300

    Add validation & hash password to register player

--- routes/auth.js
+++ routes/auth.js
@@ -1,31 +1,33 @@
-const express = require("express");
-const router = express.Router();
+const express = require("express")
+const router = express.Router()
 
-const Player = require("../models/Player");
+const Player = require("../models/Player")
+
+// const { check, validationResult } = require("express-validator")
 
 // @route GET api/auth
 // @desc Get logged in player
 // @access Private
 router.get("/", (req, res) => {
-  res.send("Get logged in player");
-});
+  res.send("Get logged in player")
+})
 
 // @route POST api/auth/login
 // @desc Auth player & get token
 // @access Public
 router.post("/login", (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
+  const username = req.body.username
+  const password = req.body.password
 
-  Player.findOne({ username }).then(player => {
+  Player.findOne({ username, password }).then((player) => {
+    console.log(player)
     if (!player) {
-      errors.username = "Wrong username or password";
-      return res.status(404).json(errors);
+      return res.status(400).json({ msg: "Ei onnistu" })
     } else {
-      res.send(Player);
-      console.log("Success");
+      res.send(player)
+      console.log("Success")
     }
-  });
-});
+  })
+})
 
-module.exports = router;
+module.exports = router
--- routes/players.js
+++ routes/players.js
@@ -1,28 +1,62 @@
-const express = require("express");
-const router = express.Router();
+const express = require("express")
+const router = express.Router()
 
-const Player = require("../models/Player");
+const Player = require("../models/Player")
+
+const { check, validationResult } = require("express-validator")
+const bcrypt = require("bcryptjs")
 
 // @route POST api/players
 // @desc Register a player
 // @access Public
-router.post("/", (req, res) => {
-  console.log(req.body);
-  const newPlayer = new Player({
-    username: req.body.username,
-    password: req.body.password
-  });
-  newPlayer.save();
-  res.send("Register a player");
-});
+router.post("/", async (req, res) => {
+  await check("username", "Username is required").not().isEmpty().run(req)
+  await check("password", "Please enter a password with 6 or more charaters")
+    .isLength({ min: 6 })
+    .run(req)
+
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+  // console.log(req.body)
+  try {
+    const player = await Player.findOne({ username: req.body.username })
+
+    if (player) {
+      return res.status(400).json({ msg: "User already exists" })
+    }
+
+    const newPlayer = new Player({
+      username: req.body.username,
+      password: req.body.password,
+    })
+
+    // const { password } = req.body
+    const salt = await bcrypt.genSalt(10)
+    newPlayer.password = await bcrypt.hash(newPlayer.password, salt)
+
+    const savedPlayer = await newPlayer.save()
+    res.send(savedPlayer)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send("Server error")
+  }
+})
 
 // @route DELETE api/players
-// @desc Register a player
+// @desc Delete a player
 // @access Public
 router.delete("/", (req, res) => {
-  res.send("Delete a player");
-});
+  //   Player.findOneAndDelete({ // id myöhemmin }).then((player) => {
+  //     console.log(player)
+  //     if (!player) {
+  //       return res.status(400).json({ msg: "Ei onnistu" })
+  //     } else {
+  //       res.send(player)
+  //       console.log("Success")
+  //     }
+  //   })
+})
 
-// Todo: Find player by email/username Player.findOne
-
-module.exports = router;
+module.exports = router
