9abb05b8d100e314da40aa7a9dfc53f22cb9f14b
Vertipae <33002993+Vertipae@users.noreply.github.com>
2020-08-19 15:09:04 +0300

    Add validation & token to login & compare hash password

--- routes/auth.js
+++ routes/auth.js
@@ -2,12 +2,17 @@ const express = require("express")
 const router = express.Router()
 
 const Player = require("../models/Player")
+const jwt = require("jsonwebtoken")
+const config = require("config")
+const { check, validationResult } = require("express-validator")
+const bcrypt = require("bcryptjs")
 
 // const { check, validationResult } = require("express-validator")
 
 // @route GET api/auth
 // @desc Get logged in player
 // @access Private
+// Validates the logged in user
 router.get("/", (req, res) => {
   res.send("Get logged in player")
 })
@@ -15,19 +20,56 @@ router.get("/", (req, res) => {
 // @route POST api/auth/login
 // @desc Auth player & get token
 // @access Public
-router.post("/login", (req, res) => {
-  const username = req.body.username
-  const password = req.body.password
+router.post("/login", async (req, res) => {
+  await check("username", "Username is required").not().isEmpty().run(req)
+  await check("password", "Password is required").exists().run(req)
 
-  Player.findOne({ username, password }).then((player) => {
-    console.log(player)
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+
+  // const username = req.body.username
+  // const password = req.body.password
+
+  const { username, password } = req.body
+
+  try {
+    const player = await Player.findOne({ username })
+
     if (!player) {
-      return res.status(400).json({ msg: "Ei onnistu" })
-    } else {
-      res.send(player)
-      console.log("Success")
+      return res.status(400).json({ msg: "Invalid Credentials" })
     }
-  })
+
+    const isMatch = await bcrypt.compare(password, player.password)
+
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Invalid Credentials" })
+    }
+
+    const payload = {
+      player: {
+        id: player.id,
+      },
+    }
+
+    // Adding token & response with Json Web Token
+    jwt.sign(
+      payload,
+      config.get("jwtSecret"),
+      {
+        expiresIn: 360000,
+        // Callback that returns token and throws possible errors
+      },
+      (err, token) => {
+        if (err) throw err
+        res.send({ token })
+      }
+    )
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send("Server error")
+  }
 })
 
 module.exports = router
