74e9f8ec6b03c1643793068bc763d01d68d7c5c9
vertipae <sallamaarit-@windowslive.com>
2020-09-17 10:14:30 +0300

    Add authActions & axiosService for Redux

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -12,3 +12,29 @@ export const registerUser = async (userData, history ) => dispatch => {
         console.log(err)
     }
 }
+
+// Login get user token
+export const loginUser = async (userData) => dispatch => {
+    try {
+        const res = await axios.post('api/auth/login', userData)
+        setToken(res.data.token)
+        const decoded = jwt_decode(token)
+        dispatch(setCurrentPlayer(decoded))
+        history.push('/home')
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+export const setCurrentPlayer = decoded => {
+    return {
+        type: SET_CURRENT_PLAYER,
+        payload: decoded
+    }
+}
+
+export const logoutUser = () => dispatch => {
+    // Remove token localStorage
+    setToken(null)
+    dispatch(setCurrentPlayer(null))
+}
