952a2734ae5c84443cc568f66ccf18e9a4f52548
vertipae <sallamaarit-@windowslive.com>
2020-11-05 15:47:35 +0200

    Add readme

--- client/src/components/layouts/WelcomeBar.js
+++ client/src/components/layouts/WelcomeBar.js
@@ -1,22 +1,26 @@
-import React from 'react'
+import React from "react"
 import { useSelector } from "react-redux"
 
 export default function WelcomeBar() {
-    const auth = useSelector((state) => state.auth.player)
-    console.log(auth)
+  const auth = useSelector((state) => state.auth.player)
+  // console.log(auth)
 
-    if(!auth) {
-        return <div></div>
-    }
-    return (
-        <div className="container" style={{ marginBottom: "1.5em"}}>
-            <h5>
-            Welcome,{'  '}<span style={{color: "Highlight",  textTransform:"capitalize"}}>{auth.player.username}</span>. {'  '}Are you ready to start your adventure?{'  '}
-            <span style={{ fontSize: "40px", color: "Mediumslateblue" }}>
-            <i className='fas fa-hat-wizard'></i>
-            </span>{'  '}
-             </h5>
-             
-        </div>
-    )
+  if (!auth) {
+    return <div></div>
+  }
+  return (
+    <div className='container' style={{ marginBottom: "1.5em" }}>
+      <h5>
+        Welcome,{"  "}
+        <span style={{ color: "Highlight", textTransform: "capitalize" }}>
+          {auth.player.username}
+        </span>
+        . {"  "}Are you ready to start your adventure?{"  "}
+        <span style={{ fontSize: "40px", color: "Mediumslateblue" }}>
+          <i className='fas fa-hat-wizard'></i>
+        </span>
+        {"  "}
+      </h5>
+    </div>
+  )
 }
